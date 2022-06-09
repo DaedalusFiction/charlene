700a2777e2f7592a069b69b6e5776b7322b2e77c
David Sorensen <teardownthismall@gmail.com>
2022-06-09 14:02:48 -0400

    added testimonial component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import { Typography } from "@mui/material";
 import Hero from "./components/Hero";
 import Bio from "./components/Bio";
 import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
 
 function App() {
+    const testimonials = [
+        {
+            speaker: "Bobby Boucher",
+            body: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate repellendus consectetur eaque exercitationem, explicabo et blanditiis commodi sed non consequuntur deserunt hic officiis distinctio labore qui accusamus repellat dicta quaerat nihil voluptates eveniet! Error excepturi quidem minus laboriosam iusto vero velit saepe magnam ipsa molestiae sequi facilis in, itaque assumenda."',
+        },
+    ];
     return (
         <>
             <Hero />
             <Bio />
             <Services />
+            <Testimonial
+                text={testimonials[0].body}
+                speaker={testimonials[0].speaker}
+            />
         </>
     );
 }
---
+++ src/components/Testimonial.js
@@ -0,0 +1,28 @@
+import { Box, Container, Typography } from "@mui/material";
+import React from "react";
+
+const Testimonial = ({ text, speaker }) => {
+    return (
+        <Container>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    padding: "7vw 0",
+                }}
+            >
+                <Box>
+                    <Typography sx={{ maxWidth: "75ch", fontStyle: "italic" }}>
+                        {text}
+                    </Typography>
+                    <Typography sx={{ textAlign: "right" }}>
+                        -{speaker}
+                    </Typography>
+                </Box>
+            </Box>
+        </Container>
+    );
+};
+
+export default Testimonial;
